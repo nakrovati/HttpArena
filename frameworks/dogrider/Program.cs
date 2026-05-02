e13c3a65ebb91066e1431640c4e57f5f52d1293e
Diogo Martins <diogoalves@ua.pt>
2026-05-02 16:11:07 +0100

    bump dogrider

--- frameworks/dogrider/Program.cs
+++ frameworks/dogrider/Program.cs
@@ -1,5 +1,6 @@
 using dogrider.Protocol;
 using dogrider.Server;
+using zerg.Engine.Configs;
 
 namespace riderdog;
 
@@ -7,11 +8,45 @@ internal static class Program
 {
     private static async Task Main()
     {
-        await using var server = new DogriderServer(
+        await using var server = new Dogrider(
+            new EngineOptions
+            {
+                Ip = "0.0.0.0",
+                Port = 8080,
+                Backlog = 65535,
+                ReactorCount = 16,
+                AcceptorConfig = new AcceptorConfig(
+                    RingFlags: 0,
+                    SqCpuThread: -1,
+                    SqThreadIdleMs: 100,
+                    RingEntries: 8 * 1024,
+                    BatchSqes: 4096,
+                    CqTimeout: 100_000_000,
+                    IPVersion: IPVersion.IPv4Only
+                ),
+                ReactorConfigs = Enumerable.Range(0, 16).Select(_ => new ReactorConfig(
+                    RingFlags: (1u << 12) | (1u << 13), // SINGLE_ISSUER | DEFER_TASKRUN
+                    SqCpuThread: -1,
+                    SqThreadIdleMs: 100,
+                    RingEntries: 1 * 1024,
+                    RecvBufferSize: 1 * 1024,
+                    BufferRingEntries: 16 * 1024,
+                    BatchCqes: 4096,
+                    MaxConnectionsPerReactor: 1 * 384,
+                    CqTimeout: 1_000_000,
+                    ConnectionBufferRingEntries: 32,
+                    IncrementalBufferConsumption: false
+                )).ToArray()
+            }, 
+            handler: new EchoHandlerPipelined());
+        
+        /*
+        await using var server = new Dogrider(
             ip: "0.0.0.0",
             port: 8080,
-            reactorCount: Math.Max(1, 64),
+            reactorCount: Math.Max(1, 16),
             handler: new EchoHandler());
+        */
 
         server.Start();
         
@@ -30,8 +65,11 @@ internal static class Program
     }
 }
 
-internal sealed class EchoHandler : Handler
+internal sealed class EchoHandlerPipelined : Handler
 {
+    
+    private static ReadOnlySpan<byte> _hello => "hello"u8;
+    
     public async ValueTask HandleAsync(IConnection connection)
     {
         while (true)
@@ -56,12 +94,14 @@ internal sealed class EchoHandler : Handler
                 {
                     case FrameType.Text:
                         
-                        connection.Write(frame.Data);
+                        //connection.Write(frame.Data);
+                        connection.Write(_hello);
                         break;
                     
                     case FrameType.Binary:
                         
-                        connection.Write(frame.Data, FrameType.Binary);
+                        //connection.Write(frame.Data, FrameType.Binary);
+                        connection.Write(_hello, FrameType.Binary);
                         break;
                     
                     case FrameType.Ping:
